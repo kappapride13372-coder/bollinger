8477d3c87e0fe8f4555e895bfb9a2678ae2b9ef1
kappapride13372-coder <kappapride13372@gmail.com>
2025-09-24 21:50:05 +0200

    connect.py

--- connect.py
+++ connect.py
@@ -3,5 +3,13 @@ from binance.client import Client
 api_key = "U8J05yLTrjPnyyjjK6PqsadNI6XGwEO53h25PyTfIKBkUpHfiLgTrOYMeyO4mRN7"
 api_secret = "zALQdNiCInvTb7OsrbNJR6pnPGHW1ULAuvMoLyo4vW83V4k78ulGeJemXJ62FDSf"
 
-client = Client(api_key, api_secret)
-print(client.get_account())  # should return account info if IP is whitelisted
+# Connect to Binance.US by specifying the testnet=False and using the US URL
+client = Client(api_key, api_secret, tld='us')  # 'tld="us"' points to Binance.US
+
+# Simple account check
+try:
+    account_info = client.get_account()
+    print("Connected to Binance.US!")
+    print(account_info)
+except Exception as e:
+    print("Connection failed:", e)
